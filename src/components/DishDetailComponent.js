b2cbe96760fef652e60d350846324f1352ab2ab9
Steve Sinai <ssinai@yahoo.com>
2018-09-06 03:29:02 -0700

    Week 1 Assignment

---
+++ src/components/DishDetailComponent.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+
+
+class DishDetail extends Component {
+
+  renderComments(comments) {
+    const listComments = comments.map((comment) =>
+      <li key={comment.id.toString()}>
+      <span><p>
+        {comment.comment}</p><p>--{comment.author},&nbsp;
+        {new Intl.DateTimeFormat('en-US',
+          {year: 'numeric', month: 'short', day: '2-digit'}).format(
+          new Date(Date.parse(comment.date)))}</p></span>
+      </li>
+    );
+
+    return (
+      <ul className="list-unstyled">{listComments}</ul>
+    );
+  }
+
+  render() {
+    return (
+      <div className="row">
+      <div className="col-12 col-md-5 m-1">
+      <Card>
+          <CardImg top src={this.props.dish.image} alt={this.props.dish.name} />
+          <CardBody>
+            <CardTitle>{this.props.dish.name}</CardTitle>
+            <CardText>{this.props.dish.description}</CardText>
+          </CardBody>
+      </Card>
+      </div>
+
+      <div className="col-12 col-md-5 m-1">
+        <h4>Comments</h4>
+        {this.renderComments(this.props.dish.comments)}
+      </div>
+      </div>
+
+    );
+  }
+}
+
+export default DishDetail;
