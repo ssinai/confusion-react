5b2ecd76cc3bb17518a02ddc07cdc00a29355354
Steve Sinai <ssinai@yahoo.com>
2018-09-07 18:38:41 -0700

    Presentation and Container Components

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 
-
 class DishDetail extends Component {
 
   renderComments(comments) {
@@ -22,25 +21,30 @@ class DishDetail extends Component {
   }
 
   render() {
-    return (
-      <div className="row">
-      <div className="col-12 col-md-5 m-1">
-      <Card>
-          <CardImg top src={this.props.dish.image} alt={this.props.dish.name} />
-          <CardBody>
-            <CardTitle>{this.props.dish.name}</CardTitle>
-            <CardText>{this.props.dish.description}</CardText>
-          </CardBody>
-      </Card>
-      </div>
+    if (this.props.dish != null) {
+      return (
+        <div className="container">
+        <div className="row">
+        <div className="col-12 col-md-5 m-1">
+        <Card>
+            <CardImg top src={this.props.dish.image} alt={this.props.dish.name} />
+            <CardBody>
+              <CardTitle>{this.props.dish.name}</CardTitle>
+              <CardText>{this.props.dish.description}</CardText>
+            </CardBody>
+        </Card>
+        </div>
 
-      <div className="col-12 col-md-5 m-1">
-        <h4>Comments</h4>
-        {this.renderComments(this.props.dish.comments)}
-      </div>
-      </div>
-
-    );
+        <div className="col-12 col-md-5 m-1">
+          <h4>Comments</h4>
+          {this.renderComments(this.props.dish.comments)}
+        </div>
+        </div>
+        </div>
+      );
+    } else {
+      return <div></div>
+    }
   }
 }
 
