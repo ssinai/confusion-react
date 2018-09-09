92c1ad4d211f60bc6b24f887a8e3053c127a419a
Steve Sinai <ssinai@yahoo.com>
2018-09-09 16:20:59 -0700

    Single Page Applications Part 1

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
-import './App.css';
+import { BrowserRouter } from 'react-router-dom';
 import Main from './components/MainComponent';
+import './App.css';
 
 class App extends Component {
 
   render() {
     return (
+      <BrowserRouter>
       <div className="App">
         <Main />
       </div>
+      </BrowserRouter>
     );
   }
 }
--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -6,7 +6,7 @@ function RenderDish({dish}) {
   return (
     <div className="col-12 col-md-5 m-1">
     <Card>
-        <CardImg top src={dish.image} alt={dish.name} />
+        <CardImg width="100%" src={dish.image} alt={dish.name} />
         <CardBody>
           <CardTitle>{dish.name}</CardTitle>
           <CardText>{dish.description}</CardText>
@@ -40,12 +40,12 @@ function RenderComments({comments}) {
 const DishDetail = (props) => {
   if (props.dish != null) {
     return(
-    <div className="container">
-    <div className="row">
+      <div className="container">
+      <div className="row">
       <RenderDish dish={props.dish} />
       <RenderComments comments={props.dish.comments} />
-    </div>
-    </div>
+      </div>
+      </div>
   );
   } else {
     return (<div></div>);
